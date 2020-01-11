5dbb116f99c64c5fd4f3d9a133e3bb0db4a31283
Ustuncan4242400 <59260929+Ustuncan4242400@users.noreply.github.com>
2020-01-11 21:08:50 +0300

    Add files via upload
    CODED BY USTUNCAN4242400
    
    BU Bİ BASH BETİĞİDİR...
    BU SCRİPT GELİŞTİRİLECEKTİR...

---
+++ TURKHAREKATİM.sh
@@ -0,0 +1,336 @@
+#! bin/bash
+#CODED BY STAR_SKY
+echo -e '
+________________$$$$$$$$$$$$$$__________________________________ 
+'
+sleep 0.5
+echo -e '
+_____________$$$$$$$$$$$$$$$$$_________________________________ 
+'
+sleep 0.5
+echo -e '
+____________$$$$$$$$$$$$$$$$$$$________________________________ 
+'
+sleep 0.5
+echo -e '
+__________$$$$$$$$$$$$$$$$$$$$$$$______________________________ 
+'
+sleep 0.5
+echo -e '
+__________$$$$$$$$$$$$$$$$$$$$$$$$_____________________________ 
+'
+sleep 0.5
+echo -e '
+________$$$$$$$$$$$$$$$_____$$$$$$$____________________________ 
+'
+sleep 0.5
+echo -e '
+________$$$$$$$$$$$$___________$$$$____________________________ 
+'
+sleep 0.5
+echo -e '
+_______$$$$$$$$$$$$_____________$$$$___________________________ 
+'
+sleep 0.5
+echo -e '
+_______$$$$$$$$$$$_______________$$$___________________________ 
+'
+sleep 0.5
+echo -e '
+______$$$$$$$$$$$_________________$$$_____$$___________________ 
+'
+sleep 0.5
+echo -e '
+______$$$$$$$$$$__________________________$$$__________________ 
+'
+sleep 0.5
+echo -e '
+______$$$$$$$$$___________________________$$$__________________ 
+'
+sleep 0.5
+echo -e '
+_____$$$$$$$$$$___________________________$$$$_________________ 
+'
+sleep 0.5
+echo -e '
+_____$$$$$$$$$____________________________$$$$$$$$$$$$_________ 
+'
+sleep 0.5
+echo -e '
+_____$$$$$$$$$___________________________$$$$$$$$$$$___________ 
+'
+sleep 0.5
+echo -e '
+_____$$$$$$$$$_________________________$$$$$$$$$$$$____________ 
+'
+sleep 0.5
+echo -e '
+_____$$$$$$$$$_______________________$$$$$$$$$$$$$_____________ 
+'
+sleep 0.5
+echo -e '
+_____+$$$$$$$$_____________________$$$$$$$$$$$$$$$_____________ 
+'
+sleep 0.5
+echo -e '
+_____$$$$$$$$$_________________________$$$$$$$$$$$_____________
+'
+sleep 0.5
+echo -e '
+_____$$$$$$$$$___________________________$$$$$$$$$$____________ 
+'
+sleep 0.5
+echo -e '
+_____$$$$$$$$$$___________________________$$$$$$$$$$___________ 
+'
+sleep 0.5
+echo -e '
+______$$$$$$$$$___________________________$$$$$$$$$$___________ 
+'
+sleep 0.5
+echo -e '
+______$$$$$$$$$___________________________$$$$____$$$__________ 
+'
+sleep 0.5
+echo -e '
+_______$$$$$$$$$__________________________$$$_______$$_________ 
+'
+sleep 0.5
+echo -e '
+_______$$$$$$$$$$_________________$_______$$__________$________ 
+'
+sleep 0.5
+echo -e '
+________$$$$$$$$$$_______________$$_______$____________________ 
+'
+sleep 0.5
+echo -e '
+________$$$$$$$$$$$_____________$$$____________________________ 
+'
+sleep 0.5
+echo -e '
+_________$$$$$$$$$$$$$________$$$$_____________________________ 
+'
+sleep 0.5
+echo -e '
+__________$$$$$$$$$$$$$$$$$$$$$$$______________________________ 
+'
+sleep 0.5
+echo -e '
+___________$$$$$$$$$$$$$$$$$$$$$_______________________________ 
+'
+sleep 0.5
+echo -e '
+____________$$$$$$$$$$$$$$$$$$$________________________________ 
+'
+sleep 0.5
+echo -e '
+_____________$$$$$$$$$$$$$$$$$_________________________________ 
+'
+sleep 0.5
+echo -e '
+______________$$$$$$$$$$$$$$$__________________________________ 
+'
+sleep 0.5
+echo -e '
+_______________$$$$$$$$$$$$$___________________________________
+'
+sleep 0.1
+echo -e '
+TURK HAREKAT TİM (+ (+ (+ (+ (+ (+ (+ (+ (+ (+ TÜRK HAREKAT TİM
+'
+echo -e '\033[31;40;1m
+0=GEREKLİ KURULUMLAR
+1=TURK SPLOİT
+2=TURK-DDOS
+3=TURK DDOS 
+5=IO GEOLOCATİON
+5=XBPX
+6=İNSTASHELL
+7=İNSTABRUTE
+8=İNSHACKLE
+9=FACEBASH
+10=TWİTTER BRUTE
+11=SQLMAP
+12=REDHAWK
+13=PHİSİNG TOOLS
+14=IPs-Finder
+15=SLOWLORİS
+16=UFONET
+17=BOTNET ATTACK (BOTNET SALDIRISI)
+18=DDOS (HAMMER)
+19=PENTMENU
+20=FAKEROOT DEBİAN
+21=ÇIKIŞ
+'
+read -p 'İŞLEM SEÇİNİZ=>' secim
+
+if [[ $secim == 0 || secim == 00 ]]; then
+	pkg install tor -y
+	pkg install curl -y
+	pkg install wget -y
+	pkg install python python2 -y
+	pkg install clang -y
+	pkg install lolcat -y
+	pkg install sl -y
+	pkg install openssl-tool -y
+	apt update -y
+	apt upgrade -y
+	pip install selenium
+	pip install ruby
+	pip install requests
+	pip install lolcat
+	pip install termcolor
+	pip install dnspython
+	pkg install perl -y
+
+	elif [[ $secim == 1 || $secim == 01 ]]; then
+  	cd $HOME
+	git clone https://github.com/yamanefkar/Turk-Sploit
+	cd $HOME/QCNMLKQ
+	bash TURKHAREKATİM.sh
+
+elif [[ $secim == 2 || $secim == 02 ]]; then
+	cd $HOME
+	git clone https://github.com/yamanefkar/T-DDos
+	cd QCNMLKQ
+	bash TURKHAREKATİM.sh
+
+elif [[ $secim == 3 || $secim == 03 ]]; then
+	cd $HOME
+	git clone https://github.com/COLAK-C/DDoS-Tool
+	cd QCNMLKQ
+	bash TURKHAREKATİM.sh
+
+elif [[ $secim == 4 || $secim == 04 ]]; then
+	cd $HOME
+	git clone https://github.com/yamanefkar/Turk-Geolocation
+	cd QCNMLKQ
+	bash TURKHAREKATİM.sh
+
+elif [[ $secim == 5 || $secim == 05 ]]; then
+	cd $HOME
+	git clone https://github.com/Ustuncan4242400/XBPX
+	cd QCNMLKQ
+	bash TURKHAREKATİM.sh
+
+elif [[ $secim == 6 || $secim == 06 ]]; then
+	cd $HOME
+	git clone https://github.com/thelinuxchoice/instashell
+	cd QCNMLKQ
+	bash TURKHAREKATİM.sh
+
+elif [[ $secim == 7 || $secim == 07 ]]; then
+	cd $HOME
+	git clone https://github.com/chinoogawa/instaBrute
+	cd QCNMLKQ
+	bash TURKHAREKATİM.sh
+
+elif [[ $secim == 8 || $secim == 08 ]]; then
+	cd $HOME
+	git clone https://github.com/thelinuxchoice/inshackle
+	cd QCNMLKQ
+	bash TURKHAREKATİM.sh
+
+elif [[ $secim == 9 || $secim == 09 ]]; then
+	cd $HOME
+	git clone https://github.com/thelinuxchoice/facebash
+	cd QCNMLKQ
+	bash TURKHAREKATİM.sh
+
+elif [[ $secim == 10 ]]; then
+	cd $HOME
+	git clone https://github.com/thelinuxchoice/tweetshell
+	cd QCNMLKQ
+	bash TURKHAREKATİM.sh
+
+elif [[ $secim == 11 ]]; then
+	cd $HOME
+	git clone https://github.com/sqlmapproject/sqlmap
+	cd QCNMLKQ
+	bash TURKHAREKATİM.sh
+
+elif [[ $secim == 12 ]]; then
+	cd $HOME
+	git clone https://github.com/Tuhinshubhra/RED_HAWK.git
+	cd QCNMLKQ
+	bash TURKHAREKATİM.sh
+
+elif [[ $secim == 13 ]]; then
+	cd $HOME
+	git clone https://github.com/thelinuxchoice/shellphish
+	cd QCNMLKQ
+	bash TURKHAREKATİM.sh
+
+elif [[ $secim == 14 ]]; then
+	cd $HOME
+	git clone https://github.com/Oseid/IPs-Finder
+	cd QCNMLKQ
+	bash TURKHAREKATİM.sh
+
+elif [[ $secim == 15 ]]; then
+	cd $HOME
+	git clone https://github.com/llaera/slowloris.pl
+	cd QCNMLKQ
+	bash TURKHAREKATİM.sh
+
+elif [[ $secim == 16 ]]; then 
+	cd $HOME
+	git clone https://github.com/epsylon/ufonet
+	cd QCNMLKQ
+	bash TURKHAREKATİM.sh
+
+elif [[ $secim == 17 ]]; then
+	cd $HOME
+	git clone https://github.com/Leeon123/Python3-Botnet
+	cd QCNMLKQ
+	bash TURKHAREKATİM.sh
+
+elif [[ $secim == 18 ]]; then
+	cd $HOME
+	git clone https://github.com/cyweb/hammer
+	cd QCNMLKQ
+	bash TURKHAREKATİM.sh
+
+elif [[ $secim == 19 ]]; then
+	cd $HOME
+	git clone https://github.com/GinjaChris/pentmenu
+	cd QCNMLKQ
+	bash TURKHAREKATİM.sh
+elif [[ $secim == 20 ]]; then
+	cd $HOME
+	pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Debian/debian.sh && bash debian.sh
+	cd QCNMLKQ
+	bash TURKHAREKATİM.sh
+
+elif [[ $secim == 21 ]]; then
+	clear
+	echo -e 'TÜRK SİBER TİMİ İYİ GÜNLER DİLER^_^'
+	exit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+else
+	clear
+        echo -e ':-| HATALI GİRİŞ |-:'
+	sleep 3
+	bash TURKHAREKATİM.sh
+fi
